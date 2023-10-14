47eb92466991dbc248a666051dac188889f3014f
Shubham-Kumar-cmd <kumar53shubham@gmail.com>
2023-10-14 22:26:51 +0530

    second commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DisplayCounter from './DisplayCounter'
 import PlusOne from './PlusOne'
 import MinusOne from './MinusOne'
-import { Col, Row } from 'react-bootstrap'
+import { Button, Col, Container, Form, FormControl, Row } from 'react-bootstrap'
 import Reset from './Reset'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCounterValue } from '../store/slice/CounterSlice'
 
 const Main = () => {
 
     const counterState = useSelector((state) => state.counter.countValue)
+    const dispatch = useDispatch()
+
+    const [value, setValue] = useState(0)
+
+    const changeCounterValue = () => {
+        //action
+        // dispatch(reducer(action()))
+        dispatch(setCounterValue({ value: value }))
+    }
 
     return (
         <div>
@@ -25,8 +35,21 @@ const Main = () => {
                     <PlusOne />
                 </Col>
             </Row>
-            <div>
-                <h3>Counter: {counterState}</h3>
+            <div className='mt-3'>
+                {/* <h3>Counter: {counterState}</h3> */}
+                <Form.Group>
+                    <FormControl
+                        type='text'
+                        placeholder='enter counter value'
+                        value={value}
+                        onChange={event => setValue(event.target.value)}
+                    >
+
+                    </FormControl>
+                </Form.Group>
+                <Container className='mt-3'>
+                    <Button onClick={changeCounterValue} variant='primary'>Set Counter Value</Button>
+                </Container>
             </div>
         </div>
     )
--- src/store/slice/CounterSlice.js
+++ src/store/slice/CounterSlice.js
@@ -7,7 +7,7 @@ const initialState = {
 const CounterSlice = createSlice({
     name: "counter",//name
     initialState: initialState,//initial State
-    reducers: {//reducers functions (i.e, plusOne, miusOne, reset are the reducers functions)
+    reducers: {//reducers functions (i.e, plusOne, miusOne, reset, setCounterValue are the reducers functions)
         plusOne: (state, action) => {
             state.countValue += 1;
             return state;
@@ -19,11 +19,15 @@ const CounterSlice = createSlice({
         reset: (state, action) => {
             state.countValue = 0;
             return state;
-        }
+        },
+        setCounterValue: (state, action) => {
+            state.countValue = action.payload.value;
+            return state;
+        },
     }
 })
 
 // Action creators are generated for each case reducer function
-export const { plusOne, minusOne, reset } = CounterSlice.actions
+export const { plusOne, minusOne, reset, setCounterValue } = CounterSlice.actions
 
 export default CounterSlice.reducer;
